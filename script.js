94026331b5ab114a027384cfb1bcb77be46540c1
skidoodle <contact@albert.lol>
2023-10-26 19:01:42 +0200

    n

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function validateTAJ(taj) {
   let num = '';
 
   for (let i = 0; i < taj.length; i++) {
-    if (i < 8) {
+    if (i <= 7) {
       sum += (i + 1) % 2 == 0 ? parseInt(taj[i]) * 7 : parseInt(taj[i]) * 3;
       num += taj[i];
     }
@@ -22,13 +22,10 @@ function validateTAJ(taj) {
 function checkTAJ() {
   const tajInput = document.querySelector('#taj');
   const resultElement = document.querySelector('#result');
-  const taj = tajInput.value;
 
-  if (validateTAJ(taj)) {
-    resultElement.textContent = 'A TAJ szám érvényes.';
-    resultElement.style.color = '#4CAF50';
-  } else {
-    resultElement.textContent = 'A TAJ szám érvénytelen.';
-    resultElement.style.color = '#FF5733';
-  }
+  validateTAJ(tajInput.value)
+    ? ((resultElement.textContent = 'A TAJ szám érvényes.'),
+      (resultElement.style.color = '#4CAF50'))
+    : ((resultElement.textContent = 'A TAJ szám érvénytelen.'),
+      (resultElement.style.color = '#FF5733'));
 }
