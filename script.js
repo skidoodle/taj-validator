820c44d3772fa75d1fc9e6bf0d415d5949195bbf
skidoodle <contact@albert.lol>
2023-10-26 19:37:31 +0200

    ASd

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
-function validateTAJ(taj) {
-  taj = taj.replace(/[^0-9]/g, '');
+// Input 0
+function restrictInput(input) {
+  input.value = input.value.replace(/[^0-9]/g, '').slice(0, 9);
+}
 
-  if (taj.length != 9) {
-    return false;
+function validateTAJ(a) {
+  a = a.replace(/[^0-9]/g, '');
+  if (9 != a.length) {
+    return !1;
   }
-
-  let sum = 0;
-  let num = '';
-
-  for (let i = 0; i < taj.length; i++) {
-    if (i <= 7) {
-      sum += (i + 1) % 2 == 0 ? parseInt(taj[i]) * 7 : parseInt(taj[i]) * 3;
-      num += taj[i];
-    }
+  for (var b = 0, d = '', c = 0; c < a.length; c++) {
+    7 >= c &&
+      ((b += 0 == (c + 1) % 2 ? 7 * parseInt(a[c]) : 3 * parseInt(a[c])),
+      (d += a[c]));
   }
-
-  num += sum % 10;
-  return taj == num;
+  return a == d + (b % 10);
 }
 
 function checkTAJ() {
-  const tajInput = document.querySelector('#taj');
-  const resultElement = document.querySelector('#result');
+  var a = document.querySelector('#taj'),
+    b = document.querySelector('#result');
 
-  validateTAJ(tajInput.value)
-    ? ((resultElement.textContent = 'A TAJ szám érvényes.'),
-      (resultElement.style.color = '#4CAF50'))
-    : ((resultElement.textContent = 'A TAJ szám érvénytelen.'),
-      (resultElement.style.color = '#FF5733'));
+  validateTAJ(a.value)
+    ? ((b.textContent = 'A TAJ sz\u00e1m \u00e9rv\u00e9nyes.'),
+      (b.style.color = '#4CAF50'))
+    : ((b.textContent = 'A TAJ sz\u00e1m \u00e9rv\u00e9nytelen.'),
+      (b.style.color = '#FF5733'));
 }
